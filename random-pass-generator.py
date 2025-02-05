46f6e4dd73cd6392e41330748317e24c635282ed
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-05 14:30:47 +0530

    lowercase added

--- random-pass-generator.py
+++ random-pass-generator.py
@@ -2,6 +2,7 @@ import string, random
 
 def random_password_generator():
     length = int(input("Enter the length of your password: ").strip())
+    include_lowercase = input("Do you like to include Lower-Case characters? (y/n): ").strip().lower()
     include_uppercase = input("Do you like to include Upper-Case characters? (y/n): ").strip().lower()
     include_digits = input("Do you like to include digits? (y/n): ").strip().lower()
     include_specials = input("Do you like to include Special characters? (y/n): ").strip().lower()
@@ -10,7 +11,7 @@ def random_password_generator():
         print("Password should contains atleast 8 characters.")
         return
     
-    lowercase = string.ascii_lowercase
+    lowercase = string.ascii_lowercase if include_lowercase == 'y' else ""
     uppercase = string.ascii_uppercase if include_uppercase == 'y' else ""
     digits = string.digits if include_digits == 'y' else ""
     specials = string.punctuation if include_specials == 'y' else ""
