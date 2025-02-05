5bdcd6aea335853a0f34adb02382fd98a5307d34
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-05 14:57:25 +0530

    error handling fix

--- random-pass-generator.py
+++ random-pass-generator.py
@@ -1,16 +1,21 @@
 import string, random
 
 def random_password_generator():
-    length = int(input("Enter the length of your password: ").strip())
+    while True:
+        try:
+            length = int(input("Enter the length of your password (minimum 8): ").strip())
+            if length >= 8:
+                break
+            else:
+                print("Password should contain at least 8 characters.")
+        except ValueError:
+            print("Please enter a valid number.")
+    
     include_lowercase = input("Do you like to include Lower-Case characters? (y/n): ").strip().lower()
     include_uppercase = input("Do you like to include Upper-Case characters? (y/n): ").strip().lower()
     include_digits = input("Do you like to include digits? (y/n): ").strip().lower()
     include_specials = input("Do you like to include Special characters? (y/n): ").strip().lower()
 
-    if length < 8:
-        print("Password should contains atleast 8 characters.")
-        return
-    
     lowercase = string.ascii_lowercase if include_lowercase == 'y' else ""
     uppercase = string.ascii_uppercase if include_uppercase == 'y' else ""
     digits = string.digits if include_digits == 'y' else ""
